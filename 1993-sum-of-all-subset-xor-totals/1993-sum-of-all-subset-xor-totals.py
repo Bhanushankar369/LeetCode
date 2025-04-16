5a0f5fe7d02eaefa49e34428a9f82c28cfc98da9
Bhanushankar369 <141208242+Bhanushankar369@users.noreply.github.com>
2025-04-16 08:14:53 +0530

    Time: 0 ms (100%), Space: 17.6 MB (81.3%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
@@ -1,20 +1,23 @@
 class Solution:
-
     def subsetXORSum(self, nums: List[int]) -> int:
-        ans = []
-        def takeNotTake(self, nums, ind, lst, ans):
-            if ind == len(nums):
-                ans.append(lst[:])
-                return
-            lst.append(nums[ind])
-            takeNotTake(self, nums, ind+1, lst, ans)
-            lst.remove(nums[ind])
-            takeNotTake(self, nums, ind+1, lst, ans)
-        takeNotTake(self, nums, 0, [], ans)
-        x = 0
-        for i in range(len(ans)):
-            for j in range(1, len(ans[i])):
-                ans[i][0] ^= ans[i][j]
-            if ans[i]:
-                x += ans[i][0]
-        return x
+        # ans = []
+        # def takeNotTake(self, nums, ind, lst, ans):
+        #     if ind == len(nums):
+        #         ans.append(lst[:])
+        #         return
+        #     lst.append(nums[ind])
+        #     takeNotTake(self, nums, ind+1, lst, ans)
+        #     lst.remove(nums[ind])
+        #     takeNotTake(self, nums, ind+1, lst, ans)
+        # takeNotTake(self, nums, 0, [], ans)
+        # x = 0
+        # for i in range(len(ans)):
+        #     for j in range(1, len(ans[i])):
+        #         ans[i][0] ^= ans[i][j]
+        #     if ans[i]:
+        #         x += ans[i][0]
+        # return x
+
+        for i in range(1, len(nums)):
+            nums[0] |= nums[i]
+        return nums[0]*(2**(len(nums)-1))
