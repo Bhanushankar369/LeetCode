3d1c40a48b88a2264401fc477d47646bb4ac242f
Bhanushankar369 <141208242+Bhanushankar369@users.noreply.github.com>
2025-05-09 09:04:32 +0530

    Time: 0 ms (100%), Space: 17.7 MB (58.01%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,19 +1,25 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n <= 3:
-            return max(nums)
-        dp1 = [0]*n
-        dp1[1] = nums[1]
-        for i in range(2, n):
-            dp1[i] = nums[i] + max(dp1[:i-1])
+        def recurs(ind, nums, dp):
+            if ind == 0:
+                return nums[ind]
+            if ind < 0:
+                return 0
 
-        dp2 = [0]*n
-        dp2[0] = nums[0]
-        dp2[1] = nums[1]
-        for i in range(2, n-1):
-            dp2[i] = nums[i] + max(dp2[:i-1])
-        
-        temp1 = max(dp1)
-        temp2 = max(dp2)
-        return max(temp1, temp2)
+            if dp[ind] != -1:
+                return dp[ind]
+
+            pick = nums[ind] + recurs(ind-2, nums, dp)
+            not_pick = recurs(ind-1, nums, dp)
+
+            dp[ind] = max(pick, not_pick)
+            return dp[ind]
+        if len(nums) <= 3:
+            return max(nums)
+        n = len(nums)
+        dp1 = [-1]*n
+        first = recurs(n-2, nums[:n-1], dp1)
+        dp2 = [-1]*n
+        last = recurs(n-2, nums[1:], dp2)
+
+        return max(first, last)
