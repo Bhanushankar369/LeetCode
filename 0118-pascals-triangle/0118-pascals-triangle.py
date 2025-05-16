d36df7d3f25fa156c4b75a50736f97668c1740ec
Bhanushankar369 <141208242+Bhanushankar369@users.noreply.github.com>
2025-05-16 18:59:58 +0530

    Time: 0 ms (100%), Space: 17.7 MB (58.06%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,18 +1,10 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        def helper(i, j):
-            res = 1
-            for l in range(j):
-                res *= (i-l)
-                res /= (l+1)
-            return int(res)
         ans = [[1]]
         for i in range(1, numRows):
-            lst = []
-            for j in range(i+1):
-                if(j==0 or j==i):
-                    lst.append(1)
-                else:
-                    lst.append(helper(i, j))
-            ans.append(lst)
+            level = [1]
+            for j in range(1, i):
+                level.append(ans[i-1][j-1] + ans[i-1][j])
+            level.append(1)
+            ans.append(level)
         return ans
