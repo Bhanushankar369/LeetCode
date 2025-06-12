ec2d2b308deb8d619e0f990bb52a4ceea63f38d4
Bhanushankar369 <141208242+Bhanushankar369@users.noreply.github.com>
2025-06-12 23:26:20 +0530

    Time: 8 ms (6.28%), Space: 17.8 MB (47.93%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -21,21 +21,3 @@ class Solution:
             if result > 2**31 and sign == -1: return -2**31
 
         return sign * result
-
-
-#  s = s.strip()
-#   if not s: return 0
-#   sign = 1
-#   i = 0
-#   if s[0] == '-':
-#     sign = -1
-#     i += 1
-#   elif s[0] == '+':
-#     i += 1
-#   result = 0
-#   while i < len(s) and s[i].isdigit():
-#     result = result * 10 + int(s[i])
-#     i += 1
-#     if result > 2**31 - 1 and sign == 1: return 2**31 - 1
-#     if result > 2**31 and sign == -1: return -2**31
-#   return sign * result
