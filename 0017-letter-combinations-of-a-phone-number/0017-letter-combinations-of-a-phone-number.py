ee132c15149a37c1136e81baaea9abb895d2325f
Bhanushankar369 <141208242+Bhanushankar369@users.noreply.github.com>
2025-06-15 22:28:13 +0530

    Time: 0 ms (100%), Space: 17.7 MB (68.17%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,16 +1,21 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        import itertools
+        res = []
         map = {'1':"", '2':"abc", '3':"def",
               '4':"ghi",  '5':"jkl",  '6':"mno",  
               '7':"pqrs",  '8':"tuv",  '9':"wxyz"
               }
-        if not digits:
-            return []
-        words = []
-        for num in digits:
-            words.append(map[num])
 
-        combinations = [''.join(combo) for combo in itertools.product(*words)]
+        def backtrack(i, curr):
+            if len(curr) == len(digits):
+                res.append(curr)
+                return
 
-        return combinations 
+            for c in map[digits[i]]:
+                backtrack(i+1, curr+c)
+
+        backtrack(0, "")
+
+        if digits:
+            return res
+        return []
