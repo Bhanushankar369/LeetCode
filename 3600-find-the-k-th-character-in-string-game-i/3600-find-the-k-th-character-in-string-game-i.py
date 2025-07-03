aad8cfbffcb637121598ba576a9d376143191e91
Bhanushankar369 <141208242+Bhanushankar369@users.noreply.github.com>
2025-07-03 09:02:58 +0530

    Time: 12 ms (37.29%), Space: 17.9 MB (17.19%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
@@ -2,9 +2,11 @@ class Solution:
     def kthCharacter(self, k: int) -> str:
         alice = "a"
         while len(alice) < k:
+            temp = ""
             for c in alice:
                 if c=='z':
-                    alice += 'a'
+                    temp += 'a'
                 else:
-                    alice += chr(ord(c)+1)
+                    temp += chr(ord(c)+1)
+            alice += temp
         return alice[k-1]
