7a41c3fae8b5073e4aa9ca6ee97f084cce7bdad6
Bhanushankar369 <141208242+Bhanushankar369@users.noreply.github.com>
2025-07-14 08:48:34 +0530

    Time: 3 ms (18.99%), Space: 17.7 MB (81.07%) - LeetHub

--- 1547-destination-city/1547-destination-city.py
+++ 1547-destination-city/1547-destination-city.py
@@ -1,13 +1,10 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
         map = defaultdict(list)
-        visit = set()
         for u, v in paths:
             map[u].append(v)
-            visit.add(u)
-            visit.add(v)
-        for item in visit:
-            if len(map[item]) == 0:
-                return item
+        for u,v in paths:
+            if v not in map:
+                return v
         
         return "Kakinada"
