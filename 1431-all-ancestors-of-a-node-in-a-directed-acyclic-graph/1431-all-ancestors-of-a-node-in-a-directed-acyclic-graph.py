4947a26d65b774a2580e4b934aa4c253238e0a04
Bhanushankar369 <141208242+Bhanushankar369@users.noreply.github.com>
2025-07-29 14:19:01 +0530

    Time: 47 ms (98.25%), Space: 34.8 MB (79.83%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -14,6 +14,7 @@ class Solution:
             for nei in map[node]:
                 ancestor.add(nei)
                 ancestor.update(dfs(nei))
+                # Update is used to add group of elements without repetition
 
             dp[node] = sorted(ancestor)
             return dp[node]
