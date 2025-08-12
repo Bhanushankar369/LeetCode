4b84340a50f1a01931b1fa11ae283d0a9aa2d825
Bhanushankar369 <141208242+Bhanushankar369@users.noreply.github.com>
2025-08-12 23:33:00 +0530

    Time: 15 ms (54.17%), Space: 31.6 MB (28.65%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,8 +1,3 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        map = collections.Counter(nums)
-
-        for _, val in map.items():
-            if val > 1:
-                return True
-        return False
+        return len(set(nums)) != len(nums)
