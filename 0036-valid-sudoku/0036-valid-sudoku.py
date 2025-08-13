e6e4ecedddb6af333394082bb3446eb9c82de440
Bhanushankar369 <141208242+Bhanushankar369@users.noreply.github.com>
2025-08-13 15:52:49 +0530

    Time: 7 ms (36.71%), Space: 17.9 MB (24.89%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,35 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-        def isSafe(row, col, target):
-            if board[row][col] == '.':
-                return True
-
-            # Check the same row
-            for i in range(len(board)):
-                if i != col and board[row][i] == target:
-                    return False
-            
-            # Check the same col
-            for j in range(len(board)):
-                if j != row and board[j][col] == target:
-                    return False
-
-            # Check the same cell i.e, 3x3 grid
-            r = row - (row%3)
-            c = col - (col%3)
-
-            for x in range(r, r+3):
-                for y in range(c, c+3):
-                    if (x != row or y != col) and board[x][y] == target:
-                        return False
-                
-            return True
+        row = defaultdict(list)
+        col = defaultdict(list)
+        box = defaultdict(list)
 
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if not isSafe(i, j, board[i][j]):
+                if board[i][j] == ".":
+                    continue
+                    
+                r = i - (i%3)
+                c = j - (j%3)
+
+                if board[i][j] in row[i] or board[i][j] in col[j] or board[i][j] in box[(r, c)]:
                     return False
+
+                row[i].append(board[i][j])
+                col[j].append(board[i][j])
+                box[(r, c)].append(board[i][j])
 
         return True
         
