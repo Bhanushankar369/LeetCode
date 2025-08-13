c57218ed02ce997b5058de030d5604422c630e62
Bhanushankar369 <141208242+Bhanushankar369@users.noreply.github.com>
2025-08-13 13:04:55 +0530

    Time: 2 ms (64.4%), Space: 18 MB (28.07%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
@@ -1,16 +1,4 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        s = "balloon"
-
         map = Counter(text)
-        count = 0
-
-        while True:
-            for char in s:
-                if char not in map or map[char] < 1:
-                    return count
-                
-                map[char] -= 1
-        
-            count += 1
-        return count
+        return min(map['b'], map['a'], map['l']//2, map['o']//2, map['n'])
