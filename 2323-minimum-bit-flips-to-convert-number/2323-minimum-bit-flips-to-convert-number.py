c1c4455d3fc4be80eb1af4119e6004eaea7cd8bc
Bhanushankar369 <141208242+Bhanushankar369@users.noreply.github.com>
2025-08-30 19:01:38 +0530

    Time: 0 ms (100%), Space: 18 MB (12.88%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
@@ -3,8 +3,16 @@ class Solution:
         n = start ^ goal
         count = 0
 
-        while n!=0:
-            n = n&(n-1)
-            count += 1
+        # Counting number of 1's in the result
+        # while n!=0:
+        #     n = n&(n-1)
+        #     count += 1
+
+        # Another way of counting 1's
+        while n>1:
+            count += n&1
+            n = n>>1
+        
+        if n==1: count += 1
 
         return count
