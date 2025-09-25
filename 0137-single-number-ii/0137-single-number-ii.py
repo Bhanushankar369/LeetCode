5fc798a77959e80681aba9353b83ad24ba3a8dff
Bhanushankar369 <141208242+Bhanushankar369@users.noreply.github.com>
2025-09-25 07:15:26 +0530

    Time: 31 ms (23.66%), Space: 19.3 MB (42.83%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,8 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        map = Counter(nums)
-        for key, val in map.items():
-            if val == 1:
-                return key
-        
-        return -1
+        ans = 0
+        for bitIndex in range(32):
+            count = 0
+            for n in nums:
+                if (n & (1 << bitIndex)): # Check if bitIndex bit is set or not
+                    count += 1
+
+            if count%3 == 1:
+                ans = ans | (1 << bitIndex) # Set the bitIndex bit
+
+        # To handle negative integers
+        if ans >= (1 << 31):
+            ans -= (1 << 32)
+
+        return ans
