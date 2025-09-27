e1183f257b5dfb60031148e3196db74b011a775a
Bhanushankar369 <141208242+Bhanushankar369@users.noreply.github.com>
2025-09-27 06:35:55 +0530

    Time: 4 ms (31.63%), Space: 19.1 MB (65.64%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,10 +1,18 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        map = Counter(nums)
-        lst = []
+        num = nums[0]
+        for i in range(1, len(nums)):
+            num ^= nums[i]
 
-        for k, v in map.items():
-            if v == 1:
-                lst.append(k)
+        right_most = (num & num-1) ^ num
 
-        return lst
+        b1 = 0
+        b2 = 0
+
+        for i in range(len(nums)):
+            if nums[i] & right_most:
+                b1 ^= nums[i]
+            else:
+                b2 ^= nums[i]
+
+        return [b1, b2]
