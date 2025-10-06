34f33c99045ad80a4cd04c3d27ffa9d03f2af323
Bhanushankar369 <141208242+Bhanushankar369@users.noreply.github.com>
2025-10-06 07:18:00 +0530

    Time: 35 ms (45.9%), Space: 18.6 MB (38.11%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.py
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.py
@@ -11,25 +11,25 @@ class Solution:
 
         directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
 
-        for i in range(n):
-            for j in range(n):
-                while heap:
-                    dist, (r, c) = heapq.heappop(heap)
+        # for i in range(n):
+        #     for j in range(n):
+        while heap:
+            dist, (r, c) = heapq.heappop(heap)
 
-                    for row, col in directions:
-                        nr = row+r
-                        nc = col+c
+            for row, col in directions:
+                nr = row+r
+                nc = col+c
 
-                        if (nr < 0 or nr >= n or
-                            nc < 0 or nc >= n or
-                            (nr, nc) in visit):
-                            continue
-                        
-                        visit.add((nr, nc))
-                        ind = nr*n + nc
+                if (nr < 0 or nr >= n or
+                    nc < 0 or nc >= n or
+                    (nr, nc) in visit):
+                    continue
+                
+                visit.add((nr, nc))
+                ind = nr*n + nc
 
-                        if time[ind] > max(dist, grid[nr][nc]):
-                            time[ind] = max(dist, grid[nr][nc])
-                            heapq.heappush(heap, (max(dist, grid[nr][nc]), (nr, nc)))
+                if time[ind] > max(dist, grid[nr][nc]):
+                    time[ind] = max(dist, grid[nr][nc])
+                    heapq.heappush(heap, (max(dist, grid[nr][nc]), (nr, nc)))
 
         return time[-1]
